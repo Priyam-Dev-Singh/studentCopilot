fee4fc7f25aa2f805aff800cbc62222a7dd23919
unknown <priyamdevsingh9@gmail.com>
2026-08-19 08:10:35 +0530

    changes to index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // API Health Check
 app.get('/', (req, res) => {
-  res.json({ message: "Student OS Backend is live!" })
+  res.json({ message: "Student OS Backend is live!" });
 });
 
 // Database Health Check
